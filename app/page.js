81453b049e19a51a2d6ddb56dfd76b367691fbe7
Mohit Yadav <mohityadavhh@gmail.com>
2026-03-24 11:49:05 +0530

    correct root project

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default page
+
+
+// git add .
+// git commit -m "first commit"
+// git branch -M main
+// git push -u origin main
